41834fca68d119e28d6d2ffe73afd2405c15bea4
saki.kitai <sakitai1206@gmail.com>
2020-10-12 10:02:59 +0000

    edit and update logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,6 +1,6 @@
 class LogsController < ApplicationController
   
-  #before_action :要設定
+  before_action :correct_user_of_log, only: [:edit, :update, :destroy]
   
   def show
     @log = Log.find(params[:id])
@@ -19,19 +19,22 @@ class LogsController < ApplicationController
       flash.now[:danger] = 'Oops!'
       render :new
     end
-    
-    
-    
   end
 
   def edit
   end
-
+  
   def update
-  end
+    if @log.update(log_params)
+      flash[:success] = 'Saved changes.'
+      redirect_to @log
+    else
+      flash.now[:danger] = 'Oops!'
+      render :edit
+    end
+  end 
 
   def destroy
-    @log = current_user.logs.find_by(id: params[:id])
     @log.destroy
     flash[:success] = "Successfully deleted the log."
     redirect_back(fallback_location: root_path)
@@ -42,4 +45,12 @@ class LogsController < ApplicationController
   def log_params
     params.require(:log).permit(:date, :weather, :wind, :temperature, :water_temperature, :start_time, :end_time, :total_time, :country, :area, :point, :entry_type, :weight, :buddy, :visibility, :current, :max_depth, :avg_depth, :start_bar, :end_bar, :bar_used, :image1, :image2, :comment)
   end 
+  
+  def correct_user_of_log
+    @log = current_user.logs.find_by(id: params[:id])
+    unless @log
+      redirect_to current_user
+    end
+  end
+  
 end
